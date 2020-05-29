694bb9fde20347f5c72e596250ef6c8aba3ad182
Administrator <admin@justins-mbp.lan>
2020-05-29 00:28:30 -0400

    test and code for non zero guessed words

--- src/GuessedWords.tsx
+++ src/GuessedWords.tsx
@@ -12,6 +12,27 @@ const GuessedWords = (props: IProps) => {
     contents = (
       <span data-test='guess-instructions'>Guess the secret word!</span>
     )
+  } else {
+    const guessedWordsRow = guessedWords.map((word, index) => (
+      <tr data-test='guessed-word' key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ))
+    contents = (
+      <div data-test='guessed-words'>
+        <h3>GuessedWords</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRow}</tbody>
+        </table>
+      </div>
+    )
   }
   return <div data-test='component-guessed-words'>{contents}</div>
 }
