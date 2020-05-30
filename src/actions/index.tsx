64e22b1beab4cb7edba8c66f7d9099aa9bf94817
Administrator <admin@justins-mbp.lan>
2020-05-30 19:10:21 -0400

    set up guessWords tests

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,5 +1,6 @@
 export const actionTypes = {
-  CORRECT_GUESS: 'CORRECT_GUESS'
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 }
 
 interface ITypes {
@@ -7,9 +8,11 @@ interface ITypes {
 }
 
 /**
- * @function correctGuess
- * @returns {object} - Action object with type 'CORRECT_GUESS
+ * Returns Redux Thunk function that dispatches GUESS_WORD action
+ * and conditionally CORRECT_GUESS action
+ * @function guessWord
+ * @param {string} guessedWord  - Guessed word
  */
-export function correctGuess(): ITypes {
-  return { type: actionTypes.CORRECT_GUESS }
+export const guessWord = (guessedWord) => {
+  return function (dispatch, getState) {}
 }
