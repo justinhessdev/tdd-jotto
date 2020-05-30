e45a21fe7d4d7431c410ccbf53d3f7c4889d3801
Administrator <admin@justins-mbp.lan>
2020-05-30 00:27:49 -0400

    set up input components and storeFactory testUtils

---
+++ src/Input.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <button />
+      </div>
+    )
+  }
+}
+const mapStateToProps = (state: any) => {
+  return {}
+}
+
+export default connect(mapStateToProps)(Input)
--- test/testUtils.tsx
+++ test/testUtils.tsx
@@ -1,5 +1,19 @@
 import { ShallowWrapper } from 'enzyme'
+import { createStore } from 'redux'
 import checkPropTypes from 'check-prop-types'
+import rootReducer from '../src/reducers'
+
+/**
+ * Create a testing store with imoprted reducers, middleware, and intial state
+ * globals: rootReducer
+ * @param {object} initialState - initial state for store
+ * @function storeFactory
+ * @returns {Store} - redux store
+ */
+export const storeFactory = (initialState: any) => {
+  return createStore(rootReducer, initialState)
+}
+
 /**
  * Returns node(s) with the given data-test attribute.
  * @param {ShallowWrapper} wrapper - Enzyme Shallow Wrapper.
