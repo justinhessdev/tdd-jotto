04ad4a639e3b27dc53e02f0753b0e03544c37e6e
Administrator <admin@justins-mbp.lan>
2020-05-30 21:08:37 -0400

    guessWord action creator, add Thunk action for guessedWord piece of state

--- src/reducers/guessedWordsReducer.tsx
+++ src/reducers/guessedWordsReducer.tsx
@@ -1,9 +1,19 @@
+import { actionTypes } from '../actions'
+
 /**
- * @function guessedWordsReducer
+ * @function guessedWordReducer
  * @param {array} state - Array of guessed words
- * @param {object} action - Action to be reduced
- * @returns {array} - New guessedWords state
+ * @param {object} action - action to be reduced
+ * @returns {array} - new guessedWords state
  */
-export default (state, action) => {
-  return null
+export default (
+  state: Array<any> = [],
+  action: { type: string; payload: object }
+): Array<any> => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload]
+    default:
+      return state
+  }
 }
